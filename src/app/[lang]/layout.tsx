4d31fde77f28b520417e5ae6ed016f7c77fbe844
ahmedmansour779 <ahmedmansour1239@gmail.com>
2024-11-24 08:27:09 +0200

    add messages in layout

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,7 +1,9 @@
 import { RootLayoutProps } from "@/src/types";
+import { NextIntlClientProvider } from "next-intl";
 
 export default async function RootLayout({ children, params }: RootLayoutProps) {
     const lang = (await params).lang
+    const messages = (await import(`../../../translation/${lang}.json`)).default;
 
     return (
         <html lang={lang}>
@@ -9,7 +11,9 @@ export default async function RootLayout({ children, params }: RootLayoutProps) 
                 className={`${lang == "ar" ? "font-arabic" : "font-english"}`}
                 dir={lang == "ar" ? "rtl" : "ltr"}
             >
-                {children}
+                <NextIntlClientProvider messages={messages}>
+                    {children}
+                </NextIntlClientProvider>
             </body>
         </html>
     );
